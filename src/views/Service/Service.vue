46d2aa1a125c3484d5df760cd40ee32952130763
seejwy <jinyoon@broadwayinc.com>
2023-07-20 14:53:45 +0900

    Update docs url

--- src/views/Service/Service.vue
+++ src/views/Service/Service.vue
@@ -13,7 +13,7 @@ template(v-else)
             Simply connect to your service and start building your website.      
 
         div.action
-            a(href="https://docs.skapi.com/the-basics/#connecting-to-your-service" target="_blank")
+            a(href="https://docs.skapi.com/getting-started/" target="_blank")
                 sui-button.lineButton(type="button") Find out More
     .container
         .titleActionsWrapper
