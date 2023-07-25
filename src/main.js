9b299286708a5a20fc0fc9ffca29132655063837
seejwy <jinyoon@broadwayinc.com>
2023-07-25 17:46:30 +0900

    Redirect 404 pages to parent page

--- src/main.js
+++ src/main.js
@@ -1,80 +1,90 @@
 import { createApp, reactive, watch } from 'vue';
 import App from './App.vue';
 import './assets/main.less';
+import Admin from './admin';
 import('@/assets/main.less');
 
-// init state
-const state = reactive({
-    user: null,
-    connection: null,
-    getServices: null,
-    services: null,
-    showVerificationNotification: false,
-    setVerificationDelay: () => {
-        localStorage.setItem(`verification_message_${state.user.user_id}`, new Date().getTime());
-        state.showVerificationNotification = false;
-    },
-    blockingPromise: new Promise(res => res())
-});
 
-// init skapi
-import Admin from './admin';
-let skapi = new Admin();
-
-let awaitConnection = skapi.getConnection().then(c => {
-    state.connection = c;
-    state.user = skapi.user;
-});
-
-watch(() => state.user, user => {
-    if (user) {
-        const ONE_DAY = 86400;
-        if (!state.user?.email_verified && (Number(localStorage.getItem(`verification_message_${state.user.user_id}`)) + ONE_DAY) < new Date().getTime()) {
-            state.showVerificationNotification = true;
-            localStorage.removeItem(`verification_message_${state.user.user_id}`);
-        }
-        state.getServices = new Promise(async res => {
-            if (!state.services) {
-                state.services = (await skapi.getServices());
-            }
-            res(state.services);
-        });
-    }
-    else {
-        state.getServices = null;
-    }
-});
-
-function storeServices() {
-    if (state.user) {
-        window.sessionStorage.setItem(state.user.user_id, JSON.stringify(state.services));
-    }
-}
-
-// attach event to save service list on close
-window.addEventListener('beforeunload', storeServices);
-window.addEventListener("visibilitychange", storeServices);
-
-// init document (will use when necessary)
-
+let awaitConnection, skapi, state;
 let desktopMedia = '(min-width: 769px)';
 const desktopSize = window.matchMedia(desktopMedia);
+const isMobileDevice = /Mobile|Android|iPhone|iPad|Windows Phone/i.test(navigator.userAgent);
 
-// init vue
-const app = createApp(App);
-let router = import('@/router/index.js');
+if(desktopSize.matches || !isMobileDevice) {
+    window.location.href = 'http://localhost:5174' + window.location.pathname;
+} else {
+    // init state
+    state = reactive({
+        user: null,
+        connection: null,
+        getServices: null,
+        services: null,
+        showVerificationNotification: false,
+        setVerificationDelay: () => {
+            localStorage.setItem(`verification_message_${state.user.user_id}`, new Date().getTime());
+            state.showVerificationNotification = false;
+        },
+        blockingPromise: new Promise(res => res())
+    });
+    
+    // init skapi
+    skapi = new Admin();
+    
+    awaitConnection = skapi.getConnection().then(c => {
+        state.connection = c;
+        state.user = skapi.user;
+    });
 
-router.then((module) => {
-    const routes = module.default;
-
-    app.use(routes);
-    app.mount('#app');
-    app.config.warnHandler = function (msg, vm, trace) {
-        return null;
-    };
-
-})
-
-const log = v => { console.log(v); return v; };
+    skapi.getProfile({ refreshToken: true }).then(u => {
+        state.connection = skapi.connection;
+        state.user = u;
+    }).catch(err => err);
+    
+    watch(() => state.user, user => {
+        if (user) {
+            const ONE_DAY = 86400;
+            if (!state.user?.email_verified && (Number(localStorage.getItem(`verification_message_${state.user.user_id}`)) + ONE_DAY) < new Date().getTime()) {
+                state.showVerificationNotification = true;
+                localStorage.removeItem(`verification_message_${state.user.user_id}`);
+            }
+            state.getServices = new Promise(async res => {
+                if (!state.services) {
+                    state.services = (await skapi.getServices());
+                }
+                res(state.services);
+            });
+        }
+        else {
+            state.getServices = null;
+        }
+    });
+    
+    function storeServices() {
+        if (state.user) {
+            window.sessionStorage.setItem(state.user.user_id, JSON.stringify(state.services));
+        }
+    }
+    
+    // attach event to save service list on close
+    window.addEventListener('beforeunload', storeServices);
+    window.addEventListener("visibilitychange", storeServices);
+    
+    // init vue
+    const app = createApp(App);
+    let router = import('@/router/index.js');
+    
+    router.then((module) => {
+        const routes = module.default;
+    
+        app.use(routes);
+        app.mount('#app');
+        app.config.warnHandler = function (msg, vm, trace) {
+            return null;
+        };
+    
+    })
+    
+    const log = v => { console.log(v); return v; };
+}
 
 export { skapi, state, awaitConnection };
--- src/views/Main/404.vue
+++ src/views/Main/404.vue
@@ -2,6 +2,24 @@
 div
     sui-flextext(max-size='48') 404: Page does not exist :(
 </template>
+<script setup>
+import { useRouter } from 'vue-router';
+
+let router = useRouter();
+
+let pathnameArray = window.location.pathname.split('/');
+let pathFound = false;
+
+while(pathnameArray.length && !pathFound) {
+    pathnameArray.pop();
+    let pathname = pathnameArray.join("/");
+    let resolvedPath = router.resolve(pathname);
+    if(resolvedPath.name && resolvedPath.name !== 'notFound') {
+        router.replace({name: resolvedPath.name, params: resolvedPath.params});
+        pathFound = true;
+    }
+}
+</script>
 <style lang="less" scoped>
 div {
     text-align: center;
